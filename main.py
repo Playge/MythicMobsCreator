b6230e822ec7a0d2ed3320b290c60d335e4d7328
Playge <54169525+Playge@users.noreply.github.com>
2020-07-02 14:00:05 +0200

    Added Mob Class
    The Class mythicmob and subclass mob have been added to create a layout for mob creation. 
    
    mythicmob.mob.save():
    This Function is used as layout for the file and the Basic Attributes of the Mob

--- main.py
+++ main.py
@@ -2,16 +2,41 @@ import discord
 
 client = discord.Client()
 
+# Reading Token from external File
+with open(file="token.txt",mode="r") as f:
+    token = f.read()
+
+# Startup Logic
 @client.event
 async def on_ready():
-    print("We have logged in as {0.user}".format(client))
+    print("MythicCreator v.1 logged in as {0.user}".format(client))
+    print("")
+    print("Creator: Playge")
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith("$Testing"):
-        await message.channel.send("Hello!")
+class mythicmob:
+    
+    class mob:
+        
+        def __init__(self, name, display, health, damage, type, armor, armor_thoughness, skills, drops):
+            self.drops = drops
+            self.display = display
+            self.name = name
+            self.health = int(health)
+            self.damage = int(damage)
+            self.type = type
+            self.armor = float(armor)
+            self.skills = skills
 
-client.run("")
+        def save(self) -> "Saving the Mob in a file":
+            filename = self.name + ".yml"
+            with open(filename, 'w+') as f:
+                f.write(self.name + ":\n  Display: " + self.display + "\n  Type: " + self.type + "\n  Health: " +
+                        str(self.health) + "\n  Damage: " + str(self.damage) + "\n  Armor:" + str(self.armor) + 
+                        "\n  Skills:\n    " + self.skills())
+
+client.run(token)
