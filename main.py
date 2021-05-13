3a2b0f5be58762c5bce36e2fcd15419a13e14081
Playge <54169525+Playge@users.noreply.github.com>
2021-05-13 21:31:16 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import discord, os, re, command
+import discord, os, re
+# TODO
+
+
 
 client = discord.Client()
 
@@ -48,42 +51,92 @@ class mythicmob:
                         "\n  Armor_toughness:" + str(self.armor_thoughness) +
                         "\n  Skills:\n    " + self.skills())
 
+def answerFormator(text):
+    modifiedResult = text[1:len(text)]
+    return modifiedResult
 
 # Setup procedure
 
 # Needed default Values
 
 question_number = int(0)
-editor_mode = bool(0)
-creator_mode = bool(0)
 mob_name = str("")
+mob_display = str("")
+mob_health = 0
+mob_damage = 0
+mob_type = str("")
+mob_armor = 0
+mob_armor_thoughness = 0
+mob_skills = ""
+mob_drops = ""
+mob_options = ""
+mob_creator_mode = False
 
+question_number = 0
 # Blank Mob Object which is being edited
-editing_mob = mythicmob.mob(name=mob_name)
+editing_mob = mythicmob.mob(name=mob_name, display=mob_display, health=mob_health, damage=mob_damage, type=mob_type, armor=mob_armor, armor_thoughness=mob_armor_thoughness, skills=mob_skills, drops=mob_drops, options=mob_options)
 # Question Protocol
 @client.event
 async def on_message(message):
-    global editor_mode
     global help_menu
-    global creator_mode
+    global question_number
     if message.author == client.user:
         return
     # Command to enter the editor mode | Editor mode shall be only activated for messages sent by the user who
     # activated it
-    if message.startswith("$mm create"):
-        await message.channel.send("You are entering Creator Mode!")
-        await message.channel.send(help_menu)
-        creator_mode = 1
-    if message.startswith("$mm edit"):
-        await message.channel.send("You are entering Editor Mode! for" + editing_mob.name())
-        await message.channel.send(help_menu)
-        creator_mode = 1
-    if message.startswith(command.select(0)) & editor_mode:
-        # Everything after "mm [command] > " is considered a command argument
-        # TODO Create function which read the arguments and sets the specified values
-        command_arguments = []
-        pass
-        # Search the message string sent by the user who entered the creator mode
+    if message.content == "$mm create":
+
+        await message.channel.send("What should the mobs internal Name be? "
+                                   "(NOTE THIS ISNT ITS DISPLAY NAME)\n\n"
+                                   "type \"$\" before your answers to the questions")
+
+        question_number = 1
+    # Question Protocol
+
+
+    # Question 1 Name
+    if message.content[0] == "$" and question_number == 1:
+
+        editing_mob.name = answerFormator(message.content)
+        print(editing_mob.name)
+
+        question_number = 2
+        await message.channel.send("Mobs Display Name?")
+    # Question 2 Display
+    if message.content[0] == "$" and question_number == 2:
+
+        editing_mob.name = answerFormator(message.content)
+        print(editing_mob.name)
+
+        question_number = 3
+    # Question 3 Health
+    if message.content[0] == "$" and question_number == 4:
+
+        editing_mob.name = answerFormator(message.content)
+        print(editing_mob.name)
+
+        question_number = 4
+        await message.channel.send("Mobs Damage?")
+    # Question 4 Damage
+    if message.content[0] == "$" and question_number == 5:
+
+        editing_mob.name = answerFormator(message.content)
+        print(editing_mob.name)
+
+        question_number = 5
+    # Question 5 Type
+    if message.content[0] == "$" and question_number == 1:
+
+        editing_mob.name = answerFormator(message.content)
+        print(editing_mob.name)
+
+        question_number = 6
+
+    if question_number == 6:
+        editing_mob.save()
+
+
+
 
 
 client.run(token)
